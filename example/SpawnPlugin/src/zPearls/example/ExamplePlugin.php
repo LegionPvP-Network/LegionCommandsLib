7adb396206b3991b7e108c710cee1935b3672ada
zPearlss <87713241+zPearlss@users.noreply.github.com>
2022-06-01 21:09:14 +0200

    Update ExamplePlugin.php

--- example/SpawnPlugin/src/zPearls/example/ExamplePlugin.php
+++ example/SpawnPlugin/src/zPearls/example/ExamplePlugin.php
@@ -11,6 +11,7 @@ class ExamplePlugin extends PluginBase
     public function onEnable(): void
     {
         $this->getServer()->getCommandMap()->register('/example1', new NoArgumentsExampleCommand($this)); //$this === Plugin === PluginBase//
+        $this->getServer()->getCommandMap()->register('/example2', new ArgumentExampleCommand($this)); //$this === Plugin === PluginBase//
     }
     
     public function onDisable(): void
