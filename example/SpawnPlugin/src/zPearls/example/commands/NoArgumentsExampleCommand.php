2cc23674798c54eec56c2a1dd5e5d7b02bca87f3
zPearlss <87713241+zPearlss@users.noreply.github.com>
2022-06-01 20:53:32 +0200

    Create NoArgumentsExampleCommand.php

---
+++ example/SpawnPlugin/src/zPearls/example/commands/NoArgumentsExampleCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace zPearls\example\commands;
+
+use zPearlss\{BaseCommand, BaseArgument}; // my lib
+
+use zPearls\example\ExamplePlugin;
+use pocketmine\command\CommandSender;
+use pocketmine\Server;
+
+class NoArgumentsExampleCommand extends BaseCommand
+{
+    
+    public function __construct(ExamplePlugin $plugin)
+    {
+        parent::__construct(HCF::getInstance(), "example1", "A command example with no arguments");
+    }
+
+    public function execute(CommandSender $sender, array $args): bool
+    {
+        $sender->sendMessage("Sexy lady <3");
+        return true;
+    }
+}
